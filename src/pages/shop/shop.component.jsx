3efa7a6ea4d499db5bf1892a3ea20c0775390f4a
jekvosm <yn4k-jek@yandex.ru>
2021-10-26 11:40:11 +0400

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import SHOP_DATA from './shop.data'
+import { Route } from 'react-router'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      collections: SHOP_DATA,
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-    return (
-      <div>
-        {collections.map(({ id, ...otherCollectionProps }) => {
-          return <CollectionPreview key={id} {...otherCollectionProps} />
-        })}
-      </div>
-    )
-  }
+const ShopPage = ({ match }) => {
+  return (
+    <div>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionID`} component={CollectionPage} />
+    </div>
+  )
 }
 
 export default ShopPage
---
+++ src/redux/shop/shop.selecters.js
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize'
+
+const selectShop = state => state.shop
+
+export const selectShopCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+)
+
+export const selectCollectiosForPreview = createSelector(
+  [selectShopCollections],
+  collections => Object.values(collections)
+)
+
+export const selectCollection = memoize(collectionUrlParam =>
+  createSelector(
+    [selectShopCollections],
+    collections => collections[collectionUrlParam]
+  )
+)
